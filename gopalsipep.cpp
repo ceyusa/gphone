bf3b24c8f6c7b687c6bce44a1b2d9d292c52affc
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2012-08-29 00:18:33 +0200

    sipep: initial code

---
+++ gopalsipep.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2012 Igalia S.L.
+ *
+ * Author: Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
+ *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1, a copy of which is found in LICENSE included in the
+ * packaging of this file.
+ */
+
+#include "gopalsipep.h"
+
+#include <ptlib.h>
+#include <sip/sip.h>
+
+class MySIPEndPoint : public SIPEndPoint
+{
+    PCLASSINFO(MySIPEndPoint, SIPEndPoint);
+
+public:
+    MySIPEndPoint(OpalManager & manager,
+                  GOpalSIPEP * sipep)
+        : SIPEndPoint (manager), m_sipep(sipep) { };
+
+private:
+    GOpalSIPEP *m_sipep;
+};
+
+G_BEGIN_DECLS
+
+enum { PROP_MANAGER = 1, PROP_LAST };
+
+struct _GOpalSIPEPPrivate
+{
+    MySIPEndPoint *sipep;
+};
+
+#define GET_PRIVATE(obj)						\
+        (G_TYPE_INSTANCE_GET_PRIVATE((obj), GOPAL_TYPE_SIP_EP, GOpalSIPEPPrivate))
+
+G_DEFINE_TYPE(GOpalSIPEP, gopal_sip_ep, G_TYPE_OBJECT)
+
+static void
+create_mysipep_instance (GOpalSIPEP *self, OpalManager *manager)
+{
+    self->priv->sipep = new MySIPEndPoint(*manager, self);
+}
+
+static void
+gopal_sip_ep_set_property(GObject *object, guint property_id,
+                          const GValue *value, GParamSpec *pspec)
+{
+    GOpalSIPEP *self;
+    OpalManager *manager;
+
+    self = GOPAL_SIP_EP (object);
+
+    switch (property_id) {
+    case PROP_MANAGER:
+        if (!self->priv->sipep) {
+            manager = static_cast<OpalManager *> (g_value_get_pointer (value));
+            create_mysipep_instance (self, manager);
+        }
+        break;
+    default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+        break;
+    }
+}
+
+
+static void
+gopal_sip_ep_finalize (GObject *object)
+{
+    G_OBJECT_CLASS(gopal_sip_ep_parent_class)->finalize(object);
+}
+
+static void
+gopal_sip_ep_class_init (GOpalSIPEPClass *klass)
+{
+    GObjectClass *gobject_class = (GObjectClass *) klass;
+
+    gobject_class->finalize = gopal_sip_ep_finalize;
+    gobject_class->set_property = gopal_sip_ep_set_property;
+
+    g_type_class_add_private (klass, sizeof (GOpalSIPEPPrivate));
+
+    g_object_class_install_property (gobject_class, PROP_MANAGER,
+        g_param_spec_pointer("manager", "mgr", "Opal's Manager",
+                             GParamFlags (G_PARAM_WRITABLE |
+                                          G_PARAM_CONSTRUCT_ONLY |
+                                          G_PARAM_STATIC_NAME)));
+}
+
+static void
+gopal_sip_ep_init (GOpalSIPEP *self)
+{
+    self->priv = GET_PRIVATE (self);
+}
+
+gboolean
+gopal_sip_ep_register (GOpalSIPEP *self,
+                       GOpalSIPRegisterParams *params,
+                       gchar **address_of_record)
+{
+    gboolean ret;
+    PString aor;
+    SIPRegister::Params sip_params;
+
+    sip_params.m_addressOfRecord = params->user;
+    sip_params.m_registrarAddress = params->domain;
+    sip_params.m_authID = params->auth_name;
+    sip_params.m_password = params->password;
+    sip_params.m_expire = params->ttl;
+
+    ret = self->priv->sipep->Register (sip_params, aor);
+
+    *address_of_record = strdup((const char *) aor);
+
+    return ret;
+}
+
+G_END_DECLS
---
+++ gopalsipep.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2012 Igalia S.L.
+ *
+ * Author: Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
+ *
+ * This file may be used under the terms of the GNU Lesser General Public
+ * License version 2.1, a copy of which is found in LICENSE included in the
+ * packaging of this file.
+ */
+
+#ifndef GOPAL_SIP_EP_H
+#define GOPAL_SIP_EP_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define GOPAL_TYPE_SIP_EP			\
+    (gopal_sip_ep_get_type())
+#define GOPAL_SIP_EP(obj)			\
+    (G_TYPE_CHECK_INSTANCE_CAST((obj), GOPAL_TYPE_SIP_EP, GOpalSIPEP))
+#define GOPAL_SIP_EP_CLASS(klass)		\
+    (G_TYPE_CHECK_CLASS_CAST((klass),  GOPAL_TYPE_SIP_EP, GOpalSIPEPClass))
+#define GOPAL_IS_SIP_EP(obj)		\
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj), GOPAL_TYPE_SIP_EP))
+#define GOPAL_IS_SIP_EP_CLASS(klass)	\
+    (G_TYPE_CHECK_CLASS_TYPE((klass),  GOPAL_TYPE_SIP_EP))
+#define GOPAL_SIP_EP_GET_CLASS(obj)					\
+    (G_TYPE_INSTANCE_GET_CLASS((obj),  GOPAL_TYPE_SIP_EP, GOpalSIPEPClass))
+
+typedef struct _GOpalSIPEPPrivate GOpalSIPEPPrivate;
+typedef struct _GOpalSIPEP GOpalSIPEP;
+typedef struct _GOpalSIPEPClass GOpalSIPEPClass;
+
+struct _GOpalSIPEP {
+    GObject parent;
+
+    /*< private >*/
+    GOpalSIPEPPrivate *priv;
+};
+
+struct _GOpalSIPEPClass {
+    GObjectClass parent_class;
+};
+
+typedef struct _GOpalSIPRegisterParams GOpalSIPRegisterParams;
+
+struct _GOpalSIPRegisterParams {
+    const gchar *domain;
+    const gchar *user;
+    const gchar *auth_name;
+    const gchar *password;
+    const gchar *proxy;
+    guint ttl;
+};
+
+GType
+gopal_sip_ep_get_type                          (void) G_GNUC_CONST;
+
+gboolean
+gopal_sip_ep_register                          (GOpalSIPEP *self,
+						GOpalSIPRegisterParams *params,
+						gchar **address_of_record);
+
+G_END_DECLS
+
+#endif /* GGOPAL_SIP_EP_H */
